f4dc09c7e9f21da93201711bd022fec01d4669bc
caish156 <devlopercaish@gmail.com>
2023-12-10 00:11:33 +0530

    express_ejs

--- 10_express3.js
+++ 10_express3.js
@@ -1,25 +1,31 @@
 // express with template Engine. EJS
 
-const express = require("express");
+import express from "express";
+import timeStamp from "./timestamp.js";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+// to resolve issue with __dirname if using ES6 module.
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const server = express();
-const timestamp = require('./timestamp.js')
 
-
-
-
-
-
-
-
+// to set view engine as EJS
 server.set("view engine", "ejs");
 
+// to set path of static files like css & image files
+server.use(express.static(path.join(__dirname, "assets")));
+
+// get request to server
 server.get("/", (req, res) => {
-    res.render("index");
-     console.log(timestamp, "get/home");
+  res.render("index");
+
+  // to provide info in console on every request with timestamp.
+  console.log(timeStamp(), "GET ", "/home");
 });
 server.get("/about", (req, res) => {
-    res.render("about");
-    console.log(timestamp, "get/about");
+  res.render("about");
+  console.log(timeStamp(), "GET ", "/about");
+  // console.log(req);
 });
 server.listen(2424, () => {
   console.log("Running on port 2424");
--- timestamp.js
+++ timestamp.js
@@ -1,6 +1,12 @@
-let date_ob = new Date();
-let hours = date_ob.getHours();
-let minutes = date_ob.getMinutes();
-let seconds = date_ob.getSeconds();
+function timestamp() {
+  let ts = Date.now();
+  let date_ob = new Date(ts);
+  let hours = date_ob.getHours();
+  let minutes = date_ob.getMinutes();
+  let seconds = date_ob.getSeconds();
+  let milisecond = date_ob.getMilliseconds();
 
-let timestamp = `${hours}:${minutes}:${seconds}`;
+  return `${hours}:${minutes}:${seconds}:${milisecond}`;
+}
+
+export default timestamp;
